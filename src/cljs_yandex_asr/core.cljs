9b864749d5d385b079e54b3c82e095cb0905035d
Vladimir Epifanov <voldmar@voldmar.com>
2015-10-09 13:14:38 +0300

    Intermediate commit

--- src/cljs_yandex_asr/core.cljs
+++ src/cljs_yandex_asr/core.cljs
@@ -1,91 +1,63 @@
 (ns cljs-yandex-asr.core
   (:require
     [chord.client :refer  [ws-ch]]
-    [cljs-audiocapture :refer [capture-audio *AUDIO_FORMAT*]]
-    [cljs-uuid-utils :as uuid]
-    [cljs.core.async :as async]
-    [cljs.core.async.impl.protocols :as p])
+    [cljs.core.async :as async :refer [<! >! close! chan sliding-buffer]])
   (:require-macros
     [cljs.core.async.macros :refer [go go-loop]]))
 
 (enable-console-print!)
 
-(defn- bidi-ch
-  "Constructs bidirectional channel"
-  [read-ch write-ch & [{:keys [on-close]}]]
-  (reify
-    p/ReadPort
-    (take! [_ handler]
-      (p/take! read-ch handler))
+(defn maybe-error [message]
+  (let [message (-> (js/JSON.parse message)
+                    (js->clj :keywordize-keys true)) 
+        error? (= "Error" (.-type message))
+        data (:data message)]
+    (if error?
+      {:error (:data message)}
+      (select-keys (:data message) [:text :uttr]))))
 
-    p/WritePort
-    (put! [_ msg handler]
-      (p/put! write-ch msg handler))
-
-    p/Channel
-    (close! [_]
-      (p/close! read-ch)
-      (p/close! write-ch)
-      (when on-close
-        (on-close)))))
-
-(def ^:dynamic *asr-key*)
-(def ^:dynamic *asr-websocket* "wss://webasr.yandex.net/asrsocket.ws")
-(def ^:dynamic *retry-count* 5)
-
-
-(defn recognize* [& [asr-uuid]]
-  (let [audio-chan (async/chan)
-        results-chan (async/chan)
-        asr-chan (bidi-ch results-chan audio-chan)
-        asr-uuid (or asr-uuid (uuid/uuid-string (uuid/make-random-uuid)))
-        init-json (js/JSON.stringify (clj->js {:type "message"
+(defn init-asr [asr-url asr-key asr-uuid]
+  (let [init-json (js/JSON.stringify (clj->js {:type "message"
                                                :data {:uuid asr-uuid
-                                                      :key *asr-key*
-                                                      :format *AUDIO_FORMAT*}}))]
+                                                      :key asr-key
+                                                      :format "audio/x-pcm;bit=16;rate=44100"}}))
+        ws (<! (ws-ch asr-url {:format :str
+                               :read-ch (async/chan (sliding-buffer 16) (map maybe-error))}))
+        _ (>! (:ws-channel ws) init-json)
+        init (<! (:ws-channel ws))]
+    (merge ws init)))
+
+(defn recognize [asr-key & [{:keys [asr-uuid asr-url retry-count]
+                             :or {asr-url "wss://webasr.yandex.net/asrsocket.ws"
+                                  retry-count 5}}]]
+  (let [asr-uuid (or asr-uuid (random-uuid))
+        chans {:<audio (chan (sliding-buffer 16))
+               :>results (chan (sliding-buffer 16))}
+        {:keys [<audio >results]} chans]
+
     ; We suppose that WebSocket is not reliable
     ; so we should revive it.
-    (go-loop []
-        (let [{:keys [ws-channel error]} (async/<! (ws-ch *asr-websocket*
-                                                          {:format :str}))]
-          (if error
-            (do
-              (js/console.log "ASR socket error" error)
-              (recur))
+    (go-loop [ws (init-asr asr-url asr-key asr-uuid)
+              counter retry-count]
+      (let [{:keys [error ws-channel]} ws]
+        (if-not error
+          (do
+            ; We unconditionally send data to SpeechKit Cloud
+            (async/pipe <audio ws-channel)
+            (go-loop []
+              (let [[{:keys [message error]} _] (async/alts! [ws-channel (async/timeout 10)] {:priority true})]
+                (cond
+                  message (do
+                            (>! >results message)
+                            (recur))
+                  error (do
+                          (close! ws-channel)
+                          (>! >results {:error error}))
+                  :else (recur)))))
+          (do
+            (close! ws-channel)
+            (>! >results {:error error})
+            (when (pos? counter)
+              (recur (init-asr asr-url asr-key asr-uuid) (dec counter)))))))
+  chans))
 
-            (do
-              (async/>! ws-channel init-json)
-              (let [{:keys [message error]} (async/<! ws-channel)]
-                (if error
-                  (do
-                    (js/console.log "ASR socket error" error)
-                    (recur))
-                  (do
-                    (js/console.log "ASR init response" message)
-                    #_(async/>! audio-chan :start)
-                    (loop []
-                      (async/>! ws-channel (async/<! audio-chan))
-                      ; So, a couple words about timeout below.
-                      ; The problem is ASR web socket constantly messages us,
-                      ; even if we send no data.
-                      (let [[{:keys [message error]} c]
-                            (async/alts! [ws-channel (async/timeout 10)] {:priority true})]
-                        (if-not message
-                          (recur)
-                          (if error
-                            (js/console.log "ASR socket error" error)
-                            (let [message (-> (js/JSON.parse message)
-                                              (js->clj :keywordize-keys true)) 
-                                  error? (= "Error" (.-type message) #_(:type message))
-                                  data (:data message)]
-                              (if error?
-                                (js/console.log "ASR backend error" data)
-                                (do
-                                  (async/put! results-chan [(:text data) (:uttr data)])
-                                  (recur)))))))))))))
-          (recur)))
-  asr-chan))
-
-(defn recognize [asr-key & args]
-  (binding [*asr-key* asr-key]
-    (apply recognize* args)))
