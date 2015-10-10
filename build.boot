adee508aa7d9f78aef7d425491161c21a48a02d2
Vladimir Epifanov <voldmar@voldmar.com>
2015-10-10 16:41:13 +0300

    Working recognizer

--- build.boot
+++ build.boot
@@ -1,32 +1,28 @@
 (set-env!
   :source-paths   #{"src"}
-  :dependencies '[[adzerk/boot-cljs       "1.7.48-5" :scope "test"]
-                  [adzerk/boot-cljs-repl  "0.2.0" :scope "test"]
-                  [adzerk/boot-reload     "0.4.0" :scope "test"]
-                  [adzerk/bootlaces       "0.1.12"]
-                  [jarohen/chord          "0.6.0"]
-                  [org.clojure/clojure    "1.7.0"]
-                  [org.clojure/clojurescript "1.7.122"]
-                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  )
+  :dependencies '[[adzerk/boot-cljs           "1.7.48-5" :scope "test"]
+                  [adzerk/bootlaces           "0.1.12"   :scope "test"]
+                  [org.clojure/clojurescript  "1.7.122"]
+                  [jarohen/chord              "0.6.0"]
+                  [com.cognitect/transit-cljs "0.8.225"] ; To turn off warning about uuid
+                  [org.clojure/core.async     "0.1.346.0-17112a-alpha"]])
 
 (require
-  '[adzerk.boot-cljs      :refer [cljs]]
-  '[adzerk.boot-cljs-repl :refer [cljs-repl start-repl]]
-  '[adzerk.boot-reload    :refer [reload]]
-  '[adzerk.bootlaces      :refer :all])
+  '[adzerk.boot-cljs :refer [cljs]]
+  '[adzerk.bootlaces :refer :all])
 
 (def +version+ "0.1.4")
 (bootlaces! +version+)
 
 (task-options!
-  push {:ensure-clean false}
-  pom  {:project     'cljs-yandex-asr
-        :version     +version+
-        :description "ClojureScript core.async interface to Yandex ASR"
-        :url "https://github.com/voldmar/cljs-yandex-asr"
-        :license {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}})
+  pom  {:project      'cljs-yandex-asr
+        :version      +version+
+        :description  "ClojureScript core.async interface to Yandex SpeechKit Cloud"
+        :url          "https://github.com/voldmar/cljs-yandex-asr"
+        :scm          {:url "https://github.com/voldmar/cljs-yandex-asr"}
+        :license      {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}})
 
 (deftask build []
-  (set-env! :source-paths #{"src"})
-  (comp (cljs :optimizations :advanced)))
+  (comp
+    (cljs :optimizations :advanced)))
+
--- src/cljs_yandex_asr/core.cljs
+++ src/cljs_yandex_asr/core.cljs
@@ -1,63 +1,87 @@
 (ns cljs-yandex-asr.core
-  (:require
-    [chord.client :refer  [ws-ch]]
-    [cljs.core.async :as async :refer [<! >! close! chan sliding-buffer]])
   (:require-macros
-    [cljs.core.async.macros :refer [go go-loop]]))
-
-(enable-console-print!)
+    [cljs.core.async.macros :refer [go go-loop]]) 
+  (:require
+    [chord.client :refer [ws-ch]]
+    [chord.channels :refer [bidi-ch]]
+    [cljs.pprint :refer [pprint]]
+    [cljs.core.async :as async :refer [<! >! close! chan sliding-buffer]]))
 
 (defn maybe-error [message]
-  (let [message (-> (js/JSON.parse message)
-                    (js->clj :keywordize-keys true)) 
-        error? (= "Error" (.-type message))
-        data (:data message)]
-    (if error?
-      {:error (:data message)}
-      (select-keys (:data message) [:text :uttr]))))
+  (if (:error message)
+    message
+    (let [message (-> message
+                      :message
+                      js/JSON.parse
+                      (js->clj :keywordize-keys true))
+          message (case (:type message)
+                    "InitResponse" {:message (:data message)}
+                    "Error" {:error (:data message)}
+                    "AddDataResponse" {:message (select-keys (:data message) [:text :uttr])}
+                    message)]
+      message)))
 
-(defn init-asr [asr-url asr-key asr-uuid]
-  (let [init-json (js/JSON.stringify (clj->js {:type "message"
-                                               :data {:uuid asr-uuid
-                                                      :key asr-key
-                                                      :format "audio/x-pcm;bit=16;rate=44100"}}))
-        ws (<! (ws-ch asr-url {:format :str
-                               :read-ch (async/chan (sliding-buffer 16) (map maybe-error))}))
-        _ (>! (:ws-channel ws) init-json)
-        init (<! (:ws-channel ws))]
-    (merge ws init)))
+(defn init-asr
+  "Make web-socket and init ASR with UUID, key and format
 
-(defn recognize [asr-key & [{:keys [asr-uuid asr-url retry-count]
-                             :or {asr-url "wss://webasr.yandex.net/asrsocket.ws"
-                                  retry-count 5}}]]
+  Return channel to read Chord’s WS channel from. Write audio chunks
+  to this WS-channel and read ASR responses"
+  [asr-url asr-key asr-uuid]
+  (let [>init (chan)]
+    (go
+      (let [init-json (js/JSON.stringify (clj->js {:type "message"
+                                                   :data {:uuid (str asr-uuid)
+                                                          :key asr-key
+                                                          :format "audio/x-pcm;bit=16;rate=44100"}}))
+            ; I want messages from ASR web-socket
+            ; be hashmap either with :error key or with payload
+            ; Because chord uses formatters I can’t use
+            ; this channel as :read-ch option and need to
+            ; make proxy for ws bidi-channel
+            wrapper (chan (async/sliding-buffer 4) (map maybe-error))
+            ws (<! (ws-ch asr-url {:format :str}))
+            ws (assoc ws :ws-channel (bidi-ch
+                                       (async/pipe (:ws-channel ws) wrapper)
+                                       (:ws-channel ws)))
+            _ (>! (:ws-channel ws) init-json)
+            init (<! (:ws-channel ws))]
+        (>! >init (merge ws init))))
+    >init))
+
+(defn recognize
+  "Recognize audio stream in real time using Yandex SpeechKit Cloud
+
+  Returns map with :>audio channel to put audio chunks and
+  :<results channel to read responses from
+
+  Every response is hash map with :text containing recognized text
+  and :uttr flag if text contain fully recognized fragment
+
+  May contain :error key with error info"
+
+  [asr-key & [{:keys [asr-uuid asr-url retry-count]
+               :or {asr-url "wss://webasr.yandex.net/asrsocket.ws"
+                    retry-count 5}}]]
   (let [asr-uuid (or asr-uuid (random-uuid))
-        chans {:<audio (chan (sliding-buffer 16))
-               :>results (chan (sliding-buffer 16))}
-        {:keys [<audio >results]} chans]
+        <audio (chan (sliding-buffer 16))
+        >results (chan (sliding-buffer 16))]
 
     ; We suppose that WebSocket is not reliable
     ; so we should revive it.
-    (go-loop [ws (init-asr asr-url asr-key asr-uuid)
+    (go-loop [{:keys [ws-channel error]} (<! (init-asr asr-url asr-key asr-uuid))
               counter retry-count]
-      (let [{:keys [error ws-channel]} ws]
-        (if-not error
-          (do
-            ; We unconditionally send data to SpeechKit Cloud
-            (async/pipe <audio ws-channel)
-            (go-loop []
-              (let [[{:keys [message error]} _] (async/alts! [ws-channel (async/timeout 10)] {:priority true})]
-                (cond
-                  message (do
-                            (>! >results message)
-                            (recur))
-                  error (do
-                          (close! ws-channel)
-                          (>! >results {:error error}))
-                  :else (recur)))))
-          (do
-            (close! ws-channel)
-            (>! >results {:error error})
-            (when (pos? counter)
-              (recur (init-asr asr-url asr-key asr-uuid) (dec counter)))))))
-  chans))
+      (if error
+        (>! >results {:error error}) 
+        (let [[errors messages] (async/split :error ws-channel)]
+          (async/pipe <audio ws-channel false)
+          (async/pipe messages >results false)
+          ; Block until error, then show error
+          (let [e (<! errors)]
+            (js/console.log "Error" (pr-str e))
+            (>! >results e))))
+      (close! ws-channel)
+      (when (pos? (dec counter))
+        (recur (<! (init-asr asr-url asr-key asr-uuid)) (dec counter))))
+    {:>audio <audio
+     :<results >results}))
 
